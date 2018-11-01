4bacbdd0601644d9860c7ab75c42db1c9068a99b
DenisMl <denismldn@gmail.com>
2018-11-01 09:47:26 +0300

    js docs

---
+++ src/dijkstra.js
@@ -0,0 +1,31 @@
+module.exports = function(graph, vertex) {
+
+  // let graph = [
+  //   [0, 2, 7, 4, 0],
+  //   [2, 0, 3, 0, 0],
+  //   [7, 3, 0, 2, 1],
+  //   [4, 0, 2, 0, 5],
+  //   [0, 0, 1, 5, 0],
+  // ];
+  let distanceMarks = [0, Infinity, Infinity, Infinity, Infinity, Infinity,];
+  let visitMarks = [false, false, false, false, false, false,];
+  // let currentVertex = 0;
+
+
+  /**
+   * Find nearest vertex
+   *
+   * @param currVertex
+   * @param graph
+   * @returns {*}
+   */
+  function selectNearestVertex(currVertex, graph) {
+    let nearestVertex;
+    return nearestVertex
+  }
+
+  function recalculateNearestDistanceMarks() {
+    //TODO: change distanceMarks
+  }
+
+};
